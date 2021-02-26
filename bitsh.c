622f960ebf6954f049731994ad912eec1f3b9147
TheDoctor314 <darsh.rajan95@gmail.com>
2021-02-26 23:16:44 +0530

    Splitting input line into tokens and print them
    strtok() is generally not recommended, instead, we have
    strtok_r() and strsep().
    strtok_r() skips over empty fields( when delims are continuous),
    if strsep() returns all the empty tokens too.
    For my use case, its better to use strtok_r().

--- bitsh.c
+++ bitsh.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "linenoise.h"
 
@@ -10,6 +13,23 @@ int main(int argc, char *argv[])
     while( (line = linenoise("bitsh> ")) != NULL)
     {
         puts(line);
+
+        /*let's break the line into tokens using strtok_r()
+         * and print them as a list
+         */
+        char *str = line, *token = NULL, *save_ptr = NULL;
+
+        for(int i = 0; ; i++, str = NULL)
+        {
+            token = strtok_r(str, " ", &save_ptr);
+            if(token == NULL)
+            {
+                //no more tokens
+                break;
+            }
+            printf("%d. %s\n", i+1, token);
+        }
+
         free(line);
     }
     return 0;
