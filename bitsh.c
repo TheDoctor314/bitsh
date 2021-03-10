64a6af320b0106b417635233e553d1b710f62b08
TheDoctor314 <darsh.rajan95@gmail.com>
2021-03-10 13:30:50 +0530

    Add history support using linenoise API

--- bitsh.c
+++ bitsh.c
@@ -22,6 +22,8 @@ int main(int argc, char *argv[])
 
     while( (line = linenoise("bitsh> ")) != NULL)
     {
+        linenoiseHistoryAdd(line);
+
         Command *cmd = parseCommand(line);
 
         int ret = runifBuiltin(cmd);
