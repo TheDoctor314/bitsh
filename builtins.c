cad0fab26843f56f7c0aff81a343e4b52476db60
TheDoctor314 <darsh.rajan95@gmail.com>
2021-03-27 18:44:15 +0530

    Comments

--- builtins.c
+++ builtins.c
@@ -1,5 +1,8 @@
 #include "builtins.h"
 
+/* Currently our cd implementation does not
+ * resolve '~' as HOME directory and takes no arguments.
+ * It is the most basic of implementations.*/
 int builtin_cd(Command *cmd)
 {
     if(chdir(cmd->args[1]) < 0)
